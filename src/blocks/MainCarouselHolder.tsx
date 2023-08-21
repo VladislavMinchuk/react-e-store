0839743bde5a606d9e6156954936e055506d5c0a
VladislavMinchuk <supervladislavgun@gmil.com>
2023-08-21 09:39:30 +0300

    Implement CarouselHolder on Home page

---
+++ src/blocks/MainCarouselHolder.tsx
@@ -0,0 +1,24 @@
+import Carousel from 'react-bootstrap/Carousel';
+import ImgH_1 from '../assets/images/shoes-img-holder.jpg';
+import ImgH_2 from '../assets/images/shoes-img-holder-2.jpg';
+import ImgH_3 from '../assets/images/shoes-img-holder-3.jpg';
+
+const MainCarouselHolder = () => {
+  const arr = [ImgH_1, ImgH_2, ImgH_3]; // Static images
+
+  return (
+    <Carousel controls={false}>
+      {arr.map((image) => (
+        <Carousel.Item>
+          <div className="img-bg-holder" style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+            height: '100vh'
+          }}></div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default MainCarouselHolder;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap"
+import MainCarouselHolder from "../blocks/MainCarouselHolder"
 
 function HomePage() {
   return (
     <div className="home-page">
       <Container>
+        <MainCarouselHolder></MainCarouselHolder>
         <h2>Home</h2>
       </Container>
     </div>
